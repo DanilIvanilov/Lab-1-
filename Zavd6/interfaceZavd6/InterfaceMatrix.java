1a2cb1966f0e94809d4c2aa6c2b7632db9264427
DanilIvanilov <91307676+DanilIvanilov@users.noreply.github.com>
2021-10-28 12:04:50 +0300

    Add files via upload

---
+++ Zavd6/interfaceZavd6/InterfaceMatrix.java
@@ -0,0 +1,10 @@
+package com.company.Zavd6.interfaceZavd6;
+
+import com.company.Zavd6.Matrix;
+
+public interface InterfaceMatrix {
+    void readMatrix();
+    Matrix multiplyMatrix(Matrix other);
+    void printMatrix();
+
+}
